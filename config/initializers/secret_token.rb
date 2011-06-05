0ca14580df8be5f646a281d19bf8d1b0de50c0bc
victmask <victmask@victmask.local>
2011-06-04 21:58:15 -0700

    generated ror project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yardmarket::Application.config.secret_token = '6fd4fc98422481e41b459d5e19b9c758556f5622c01a7604d8770308dbedfb98db844e949c783fc9f2e49cae808fe3a237465be28d9579e2cafbb6e13a012e24'
